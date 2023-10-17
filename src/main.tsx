7faca7affea7ea4d05e44a750cdbd9acb306251f
P156HAM <a.hamdi.almasalmeh@gmail.com>
2023-10-17 09:36:33 +0200

    chore: fixed browser-router

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Interval-App/">
           <App />
         </BrowserRouter>
       </Provider>
