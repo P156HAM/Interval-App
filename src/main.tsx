4a033c9d7f04379091756dbee6cb87766ef81146
P156HAM <a.hamdi.almasalmeh@gmail.com>
2023-10-17 09:54:09 +0200

    chore: fixed routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
@@ -8,9 +7,7 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename="/Interval-App/">
           <App />
-        </BrowserRouter>
       </Provider>
     </React.StrictMode>,
 );
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import LoadingScreen from './view/loadingScreen/loadingScreen';
 import Home from './view/home/home'
 import Time from './view/time/time'
@@ -7,13 +8,15 @@ import TimesUpView from './view/timesUp/timesUp'
 import BreakView from './view/break/break';
 
 const Router: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<LoadingScreen />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/time" element={<Time />} />
-    <Route path="/timesup" element={<TimesUpView />} />
-    <Route path="/break" element={<BreakView />} />
-  </Routes>
+  <BrowserRouter>
+    <Routes>
+      <Route path="Interval-App/" element={<LoadingScreen />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/time" element={<Time />} />
+      <Route path="/timesup" element={<TimesUpView />} />
+      <Route path="/break" element={<BreakView />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default Router;
